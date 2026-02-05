37e8d27343554f6065f7ac00609f7da5da2901d0
Tarcísio <damascenatarcisio@gmail.com>
2026-02-05 16:41:46 -0300

    refactor: Clean up main.dart and restore commented code; remove unused import in match_list_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
+import 'core/services/service_locator.dart';
+import 'core/themes/app_theme.dart';
+import 'providers/match_provider.dart';
+import 'providers/odds_provider.dart';
+import 'providers/team_provider.dart';
+import 'screens/home/home_screen.dart';
 
-//   await setupServiceLocator();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-//   runApp(const FootballScoutApp());
-// }
+  await setupServiceLocator();
 
-// class FootballScoutApp extends StatelessWidget {
-//   const FootballScoutApp({super.key});
+  runApp(const FootballScoutApp());
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(create: (_) => TeamProvider()),
-//         ChangeNotifierProvider(create: (_) => MatchProvider()),
-//         ChangeNotifierProvider(create: (_) => OddsProvider()),
-//       ],
-//       child: MaterialApp(
-//         title: 'Football Scout',
-//         debugShowCheckedModeBanner: false,
-//         theme: AppTheme.lightTheme,
-//         darkTheme: AppTheme.darkTheme,
-//         themeMode: ThemeMode.system,
-//         home: const HomeScreen(),
-//       ),
-//     );
-//   }
-// }
+class FootballScoutApp extends StatelessWidget {
+  const FootballScoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TeamProvider()),
+        ChangeNotifierProvider(create: (_) => MatchProvider()),
+        ChangeNotifierProvider(create: (_) => OddsProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Football Scout',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
--- lib/screens/match/match_list_screen.dart
+++ lib/screens/match/match_list_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
 import '../../providers/match_provider.dart';
 import '../../core/constants/app_constants.dart';
 import '../../widgets/match_card.dart';
